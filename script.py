2a51e4a83b75c5693e9df67dbdaf41b78b5ef572
Jakub Siwiec <jakubsiwiec.nwm@gmail.com>
2019-12-02 13:23:50 +0100

    The code was cleaned up, put into blocks and commented

--- script.py
+++ script.py
@@ -14,32 +14,13 @@ req.send(b"reset")
 req.recv()
 
 
-def set_bit(value, bit):
-    return value | (1 << bit)
-
-
-def clear_bit(value, bit):
-    return value & ~(1 << bit)
-
-
-def read_walls(pos_x, pos_y, orientation):
-    req.send(b"W" + struct.pack("2B", pos_x, pos_y) + orientation_to_byte(orientation))
-
-    # read walls as unpacking the server response
-    left, front, right = struct.unpack("3B", req.recv())
-    return {"left": left, "front": front, "right": right}
+"""
+Functions for changing the data representation
+"""
 
 
 def cell_position_to_number(pos_x, pos_y):
     return pos_y + 16 * pos_x
-
-
-def was_visited(pos_x, pos_y):
-    cell_nb = cell_position_to_number(pos_x, pos_y)
-    if walls[cell_nb] % 2 == 0:
-        return False
-    else:
-        return True
 
 
 def orientation_to_direction(orientation, left, front, right):
@@ -62,6 +43,31 @@ def orientation_to_direction(orientation, left, front, right):
         south = right
 
     return north, west, south, east
+
+
+def orientation_to_byte(orientation):
+    # 1N, 2W, 3S, 4E
+    if orientation == 1:
+        return b"N"
+    if orientation == 2:
+        return b"E"
+    if orientation == 3:
+        return b"S"
+    if orientation == 4:
+        return b"W"
+
+
+"""
+Read walls of the maze, writing map to the robot's memory, update state of it on the server
+"""
+
+
+def read_walls(pos_x, pos_y, orientation):
+    req.send(b"W" + struct.pack("2B", pos_x, pos_y) + orientation_to_byte(orientation))
+
+    # read walls as unpacking the server response
+    left, front, right = struct.unpack("3B", req.recv())
+    return {"left": left, "front": front, "right": right}
 
 
 def write_cell_walls(pos_x, pos_y, orientation):
@@ -89,48 +95,47 @@ def write_cell_walls(pos_x, pos_y, orientation):
 
         walls[cell_position_to_number(pos_x, pos_y)] = cell_byte
     # in brute force search numbers list should be used as mark how many times the cell was visited
+    # then after mapping numbers list should be cleared and used as indicator how far from the center of the labirynth is the cell
 
 
-def orientation_to_byte(orientation):
-    # 1N, 2W, 3S, 4E
-    if orientation == 1:
-        return b"N"
-    if orientation == 2:
-        return b"E"
-    if orientation == 3:
-        return b"S"
-    if orientation == 4:
-        return b"W"
+def update_state(pos_x, pos_y, orientation):
+    state = b"S" + struct.pack("2B", pos_x, pos_y) + orientation_to_byte(orientation)
+    state += b"F"
+    state += struct.pack("256B", *numbers)
+    state += b"F"
+    state += struct.pack("256B", *walls)
+    req.send(state)
+    req.recv()
 
 
-def turn(orientation, turn_dir):
-    if turn_dir == "right":
-        print("is turning right")
-        if orientation != 4:
-            return orientation + 1
-        else:
-            return 1
-    elif turn_dir == "left":
-        print("is turning left")
-        if orientation != 1:
-            return orientation - 1
-        else:
-            return 4
+"""
+Checking functions
+"""
+
+
+def was_visited(pos_x, pos_y):
+    cell_nb = cell_position_to_number(pos_x, pos_y)
+    if walls[cell_nb] % 2 == 0:
+        return False
     else:
-        return orientation
+        return True
 
 
-def move(pos_x, pos_y, orientation):
-    if orientation == 1:
-        pos_y = pos_y + 1
-    elif orientation == 2:
-        pos_x = pos_x + 1
-    elif orientation == 3:
-        pos_y = pos_y - 1
-    elif orientation == 4:
-        pos_x = pos_x - 1
+def are_all_cells_visited():
+    for i in range(16):
+        for j in range(16):
+            if was_visited(i, j):
+                continue
+            else:
+                return False
+    return True
 
-    return pos_x, pos_y
+
+"""
+Decision functions
+They are responsible for checking weights of the surrounding cells and taking the one with the
+lowest weight as the direction to turn, if it is not beyond the wall
+"""
 
 
 def get_cell_weight(pos_x, pos_y, orientation):
@@ -215,53 +220,67 @@ def choose_where_to_turn(orientation, weights, are_there_walls):
     return orient
 
 
-def are_all_cells_visited():
-    for i in range(16):
-        for j in range(16):
-            if was_visited(i, j):
-                continue
-            else:
-                return False
-    return True
+"""
+Action functions
+Functions to change robots orientation and position 
+
+"""
 
 
-def update_state(pos_x, pos_y, orientation):
-    state = b"S" + struct.pack("2B", pos_x, pos_y) + orientation_to_byte(orientation)
-    state += b"F"
-    state += struct.pack("256B", *numbers)
-    state += b"F"
-    state += struct.pack("256B", *walls)
-    req.send(state)
-    req.recv()
+def turn(orientation, turn_dir):
+    if turn_dir == "right":
+        print("is turning right")
+        if orientation != 4:
+            return orientation + 1
+        else:
+            return 1
+    elif turn_dir == "left":
+        print("is turning left")
+        if orientation != 1:
+            return orientation - 1
+        else:
+            return 4
+    else:
+        return orientation
 
 
-def test_labirynth(orientation):
-    for i in range(16):
-        for j in range(16):
-            write_cell_walls(i, j, orientation)
-            update_state(i, j, orientation)
+def move(pos_x, pos_y, orientation):
+    if orientation == 1:
+        pos_y = pos_y + 1
+    elif orientation == 2:
+        pos_x = pos_x + 1
+    elif orientation == 3:
+        pos_y = pos_y - 1
+    elif orientation == 4:
+        pos_x = pos_x - 1
+
+    return pos_x, pos_y
 
 
-# number informs about the distance of cpecific cell to the center
+# number informs about how many times was the cell visited - in brute force
+# number informs about the distance of specific cell to the center in flood fill
 # numbers[0] - SW, numbers[15] - NW, numbers[240] - SE, numbers[255] - NE
 numbers = [0] * 256
 
 # walls
-# 000NWSEP
+# 000NWSEP - byte structure
 # P - 1, E - 2, S - 4, W - 8, N - 16
 # p tells if the cell was visited, N if there is wall in the north, W west, E east, and S - south
 walls = [0] * 256
 
-# starting point
+# starting point definition
 pos_x = 0
 pos_y = 0
 orientation = 1
 
+# update state before the simulation starts
 update_state(pos_x, pos_y, orientation)
 
-# test_labirynth(1)
 
+# maximum step number to avoid infinite loop
 k = 0
+
+
 while not are_all_cells_visited() and k != 1800:
 
     write_cell_walls(pos_x, pos_y, orientation)
@@ -273,7 +292,6 @@ while not are_all_cells_visited() and k != 1800:
     print(weights)
 
     orientation = choose_where_to_turn(orientation, weights, are_there_walls)
-    # update_state(pos_x, pos_y, orientation)
 
     pos_x, pos_y = move(pos_x, pos_y, orientation)
     update_state(pos_x, pos_y, orientation)
